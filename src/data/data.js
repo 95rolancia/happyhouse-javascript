d9b22ee7f770db3f314d201b4f0b697a110feedd
jangjunhyeok <95jjangjun@gmail.com>
2021-03-15 13:13:28 +0900

    Add data

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+export { user };
+const user = [
+  {
+    id: "ssafy",
+    pwd: "ssafy1234",
+    email: "ssafy@naver.com",
+  },
+  {
+    id: "bird1",
+    pwd: "birdbird1",
+    email: "bird1@gmail.com",
+  },
+  {
+    id: "ssafy2",
+    pwd: "ssafy2222",
+    email: "ssafy2@naver.com",
+  },
+];
